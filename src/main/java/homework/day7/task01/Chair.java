eb54786de8bf52fc24cbb352abc8b8b923817c99
vkhakimau <vitali.khakimau@softswiss.com>
2024-11-28 02:51:37 +0100

    Add task 0.1

---
+++ src/main/java/homework/day7/task01/Chair.java
@@ -0,0 +1,49 @@
+package homework.day7.task01;
+
+import java.util.Objects;
+
+public class Chair {
+    private int height;
+    private int width;
+
+    public Chair(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Chair chair = (Chair) o;
+        return height == chair.height && width == chair.width;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(height, width);
+    }
+
+    @Override
+    public String toString() {
+        return "Chair{" +
+                "height=" + height +
+                ", width=" + width +
+                '}';
+    }
+}
