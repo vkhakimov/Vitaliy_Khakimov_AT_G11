673863d7c0724d1a2f34b0cca7676fd96a80b579
vkhakimau <vitali.khakimau@softswiss.com>
2024-12-13 23:49:00 +0100

    Add tasks for day 11

--- src/main/java/homework/playground/essence/material/Diesel.java
+++ src/main/java/homework/playground/essence/material/Diesel.java
@@ -6,4 +6,10 @@ public class Diesel extends Matter implements Pourable, Powerable {
     public Diesel(int mass) {
         super(mass);
     }
+
+    @Override
+    public void spread(String storeName) {
+        System.out.println("I am " + this.getClass().getSimpleName() + " and I am spreading in " + storeName);
+        System.out.println("whooh..");
+    }
 }
--- src/main/java/homework/playground/essence/material/Petrol.java
+++ src/main/java/homework/playground/essence/material/Petrol.java
@@ -6,4 +6,10 @@ public class Petrol extends Matter implements Pourable, Powerable {
     public Petrol(int mass) {
         super(mass);
     }
+
+    @Override
+    public void spread(String storeName) {
+        System.out.println("I am " + this.getClass().getSimpleName() + " and I am spreading in " + storeName);
+        System.out.println("wheeh..");
+    }
 }
