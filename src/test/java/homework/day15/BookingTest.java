7c1c6e6f959d6d0f165c5dc9d9ecd53c9d412e8a
vkhakimau <vitali.khakimau@softswiss.com>
2024-12-28 02:41:05 +0100

    Add tutorial task for day 15

--- src/test/java/homework/day15/BookingTest.java
+++ src/test/java/homework/day15/BookingTest.java
@@ -1,9 +1,5 @@
-package classwork.day15;
+package homework.day15;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
@@ -11,6 +7,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -30,7 +30,7 @@ public class BookingTest {
         return formattedDate;
     }
 
-    @Before
+    @BeforeTest
     public void initialize() {
         driver = new ChromeDriver();
         driver.get("https://www.booking.com/");
@@ -103,7 +103,7 @@ public class BookingTest {
         Assert.assertEquals("Wrong rating!", rating, "1 out of 5");
     }
 
-    @After
+    @AfterTest
     public void shutDown() {
         driver.quit();
     }
